b27394da3e8c75ddaae56de1ec8c9cd7c8fe7ccf
sergiogalici <galici.sergio@gmail.com>
2023-03-03 08:38:10 +0100

    small tweaks to saga

--- src/features/products/saga.ts
+++ src/features/products/saga.ts
@@ -2,14 +2,15 @@ import { takeLatest, delay, put } from "redux-saga/effects";
 import { withLoading } from "../loading/saga";
 import { productActions } from "./reducer";
 
-function* fetchItemsSaga() {
+function* fetchProductsSaga() {
   yield delay(3000);
+  console.log("we are in fetch products")
   yield put(productActions.addProduct({ title: "new item" }));
 }
 
 export function* itemsSaga() {
   yield takeLatest(
     productActions.fetchProducts.toString(),
-    withLoading("items", fetchItemsSaga)
+    withLoading("items", fetchProductsSaga)
   );
 }
